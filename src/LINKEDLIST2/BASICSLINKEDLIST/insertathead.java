1cba6475fa7b3e854cc8162979cd062389c4a11d
rishav08521 <rishavrupesh6018@gmail.com>
2024-01-07 15:49:30 +0530

    insertionathead

--- src/LINKEDLIST2/BASICSLINKEDLIST/insertathead.java
+++ src/LINKEDLIST2/BASICSLINKEDLIST/insertathead.java
@@ -8,7 +8,48 @@ public class insertathead {
             this.data=data;
         }
     }
+    public static class linkedlist1{
+        Node head=null;
+        Node tail=null;
+        void insertathead(int val){
+            Node temp=new Node(val);
+            if (head==null){
+                head=tail=temp;
+            }
+            else {
+                temp.next=head;
+                head=temp;
+            }
+        }
+        void display(){
+            Node temp=head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+
+        }
+        int size(){
+            Node temp=head;
+            int count=0;
+            while(temp!=null){
+                count++;
+                temp=temp.next;
+
+            }
+            return count;
+        }
+    }
     public static void main(String[] args) {
+        linkedlist1 ll=new linkedlist1();
+        ll.insertathead(2);
+        ll.insertathead(3);
+        ll.insertathead(4);
+        ll.insertathead(5);
+        ll.insertathead(6);
+        ll.display();
+        System.out.println();
+        System.out.println(ll.size());
 
     }
 }
