e15ba74ea809ad1291cab67e4c4b19115e28f9d0
rishav08521 <rishavrupesh6018@gmail.com>
2024-01-14 23:18:21 +0530

    array

---
+++ src/ARRAYLIST12/basicarraylist.java
@@ -0,0 +1,17 @@
+package ARRAYLIST12;
+
+import java.util.ArrayList;
+
+public class basicarraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> at=new ArrayList<>(5);
+        at.add(2);
+        at.add(3);
+        at.add(4);
+        at.add(5);
+        at.add(6);
+//        for (int i=1;i<at.size();i++){
+//            System.out.println(at);
+        System.out.println(at);
+    }
+}
